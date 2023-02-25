4a4aaf71720cb528e611c10ddfc2efdb1eaf1de6
Abhishek-Ram25 <aramak21@asu.edu>
2023-02-24 21:32:15 -0700

    Basic Implementation of Feature-1

--- src/main/java/aramak21/DemoApp.java
+++ src/main/java/aramak21/DemoApp.java
@@ -1,12 +1,63 @@
 package aramak21;
 
+import com.paypal.digraph.parser.GraphEdge;
+import com.paypal.digraph.parser.GraphNode;
+import com.paypal.digraph.parser.GraphParser;
+import guru.nidi.graphviz.attribute.Color;
+import guru.nidi.graphviz.attribute.Font;
+import guru.nidi.graphviz.attribute.Rank;
+import guru.nidi.graphviz.attribute.Style;
+import guru.nidi.graphviz.engine.Format;
+import guru.nidi.graphviz.engine.Graphviz;
+import guru.nidi.graphviz.model.Graph;
+
+import java.io.File;
+import java.util.Map;
+
+import static guru.nidi.graphviz.attribute.Attributes.attr;
+import static guru.nidi.graphviz.attribute.Rank.RankDir.LEFT_TO_RIGHT;
+import static guru.nidi.graphviz.model.Factory.graph;
+import static guru.nidi.graphviz.model.Factory.node;
+import static guru.nidi.graphviz.model.Link.to;
+
 public class DemoApp {
 
     public static void main(String[] args) throws Exception {
 
         System.out.println("Software QA and Testing Part-1");
+
+        //parsing graph
         GraphManager gm = new GraphManager();
+
         gm.parseGraph("testInputGraph.dot");
+
+        System.out.println("The code for the graph is " + gm.toString());
+
+        gm.outputGraph("testInputGraph.dot");
+
+        gm.addNode("e");
+
+        gm.addEdge("e","c");
+
+        gm.removeNode("e");
+
+        gm.removeEdge("a","c");
+
+        //
+
+//        Graph g = graph("example1").directed()
+//                .graphAttr().with(Rank.dir(LEFT_TO_RIGHT))
+//                .linkAttr().with("class", "link-class")
+//                .with(
+//                        node("a").with(Color.RED).link(node("b")),
+//                        node("b").link(
+//                                to(node("c")).with(attr("weight", 5), Style.DASHED)
+//                        )
+//                );
+//        Graphviz.fromGraph(g).height(100).render(Format.PNG).toFile(new File("example/ex1.png"));
+
+
     }
+
 
 }
--- src/main/java/aramak21/GraphManager.java
+++ src/main/java/aramak21/GraphManager.java
@@ -1,25 +1,69 @@
 package aramak21;
 
+import guru.nidi.graphviz.engine.Format;
+import guru.nidi.graphviz.engine.Graphviz;
+import guru.nidi.graphviz.model.Link;
 import guru.nidi.graphviz.model.MutableGraph;
+import guru.nidi.graphviz.model.MutableNode;
 import guru.nidi.graphviz.parse.Parser;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 
 public class GraphManager {
+    MutableGraph g;
     public void parseGraph(String filename) throws Exception
     {
       try {
           //InputStream dot = getClass().getResourceAsStream(filename);
           FileInputStream input
                   = new FileInputStream(filename);
-          MutableGraph g = new Parser().read(input);
+           g = new Parser().read(input);
 
-          System.out.println("The graph " + g);
+          System.out.println("the number of nodes in the graph is  " + g.nodes().size());
+
+          System.out.println("the number of edges in the graph is " + g.edges().size());
+
+          for (MutableNode node : g.nodes()) {
+              System.out.println("The name of the node is " + node.name());
+
+          }
+          for (Link edge : g.edges()) {
+              System.out.println("the edge is from " + edge.from().name() + " -> " + edge.to().name());
+          }
+
+         // System.out.println(g.toString());
       }catch (Exception e)
       {
           throw e;
       }
+    }
+
+   @Override public String toString()
+    {
+     return g.toString();
+    }
+
+    public void outputGraph(String filepath) throws IOException {
+        try {
+            FileInputStream input
+                    = new FileInputStream(filepath);
+            Graphviz.fromGraph(g).height(100).render(Format.PNG).toFile(new File("example/ex1.png"));
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+    public void addEdge(String source, String target) {
+
+    }
+
+    public void addNode(String node) {
+    }
+
+    public void removeNode(String node) {
+    }
+
+    public void removeEdge(String source, String target) {
     }
 }
