3afffbd6b23f4619c37a700dad7a1fa0895c27c5
Abhishek-Ram25 <aramak21@asu.edu>
2023-02-26 17:52:20 -0700

    Implemented all features making it interactive

--- src/main/java/aramak21/DemoApp.java
+++ src/main/java/aramak21/DemoApp.java
@@ -10,6 +10,7 @@ import guru.nidi.graphviz.model.Graph;
 
 import java.io.File;
 import java.util.Map;
+import java.util.Scanner;
 
 import static guru.nidi.graphviz.attribute.Attributes.attr;
 import static guru.nidi.graphviz.attribute.Rank.RankDir.LEFT_TO_RIGHT;
@@ -23,32 +24,116 @@ public class DemoApp {
 
         System.out.println("Software QA and Testing Part-1");
 
+
         //parsing graph
         GraphManager gm = new GraphManager();
+        System.out.println(" Enter the dot file to parse the graph:");
+        String dotFileName =  new Scanner(System.in).nextLine();
+        gm.parseGraph(dotFileName);
 
-        gm.parseGraph("testInputGraph.dot");
+        int choice=0;
+        while(choice!=11)
+        {
+            System.out.println(" \nSelect an option");
+            System.out.println("1.  Print info about the graph\n"
+                                + "2.Write the information of the graph into a new file\n"
+                                + "3. Insert a new node\n"
+                                + "4. Insert many nodes into the graph\n"
+                                + "5. Remove a node from the graph\n"
+                                +  "6.Remove a number of nodes from the graph\n"
+                                +  "7. Add an edge between two nodes\n"
+                                +  "8. Remove an edge between two nodes\n"
+                                +  "9.Return the graph info in a DOT file\n" +
+                                   "10.Return the graph as an SVG or PNG\n" +
+                                   "11. Quit Program" );
 
+            choice =  new Scanner(System.in).nextInt();
 
-//        gm.outputGraph("testInputGraph.dot");
+            switch(choice)
+            {
+                case 1: gm.toString(); break;
+                case 2 : System.out.println("Enter the filename to wish you want the info to be copied to ");
+                         String filename = new Scanner(System.in).nextLine();
+                         gm.outputGraph(filename);
+                         break;
+
+                case 3: System.out.println("Enter the node to be inserted");
+                        String nodeName = new Scanner(System.in).nextLine();
+                        gm.addNode(nodeName);
+                        break;
+
+                case 4: System.out.println("Enter a list of nodes to be inserted with a [space] separating them ");
+                        String nodes = new Scanner(System.in).nextLine();
+                        gm.addNodes(nodes.split(" "));
+                        break;
+
+                case 5: System.out.println(" Enter the node to be removed");
+                        String rem_node= new Scanner(System.in).nextLine();
+                        gm.removeNode(rem_node);
+                        break;
+
+                case 6: System.out.println("Enter a list of nodes to be deleted with a [space] separating them ");
+                        String rem_nodes = new Scanner(System.in).nextLine();
+                        gm.removeNodes(rem_nodes.split(" "));
+                        break;
+
+                case 7 : System.out.println("Enter the source node");
+                         String addSource = new Scanner(System.in).nextLine();
+                         System.out.println("Enter the target");
+                         String addTarget = new Scanner(System.in).nextLine();
+                         gm.addEdge(addSource,addTarget);
+                         break;
+
+                case 8 :System.out.println("Enter the source node");
+                        String removeSource = new Scanner(System.in).nextLine();
+                        System.out.println("Enter the target");
+                        String removeTarget = new Scanner(System.in).nextLine();
+                        gm.removeEdge(removeSource,removeTarget);
+                        break;
+
+                case 9 : System.out.println("Enter the file name for the dot Graph");
+                         String dotFile = new Scanner(System.in).nextLine();
+                         gm.outputDOTGraph(dotFile);
+                         break;
+
+                case 10 : System.out.println("Enter the desired format - SVG or PNG");
+                          String fileType = new Scanner(System.in).nextLine();
+                          System.out.println("Enter the desired file name");
+                          String fileName = new Scanner(System.in).nextLine();
+                          gm.outputGraphics(fileName,fileType);
+                          break;
+
+                case 11 : System.out.println("exiting now ");
+                          break;
+
+            }
+        }
+//        //gm.parseGraph("testInputGraph.dot");
 //
-        gm.addNode("e");
-
-       gm.addNodes(new String[]{"e", "f", "g"});
-
-       gm.removeNode("a");
-
-       gm.removeNodes(new String[]{ "e","f","g"});
-
-       gm.toString();
-
-        gm.addEdge("a","e");
-        gm.addEdge("f","g");
-
-        gm.toString();
-
-        gm.removeEdge("a","e");
-
-        gm.toString();
+//        gm.addNode("e");
+//
+//       gm.addNodes(new String[]{"e", "f", "g"});
+//
+//       gm.removeNode("a");
+//
+//       gm.removeNodes(new String[]{ "e","f","g"});
+//
+//        gm.addEdge("a","e");
+//        gm.addEdge("a","c");
+//        gm.addEdge("a","f");
+//        gm.addEdge("f","g");
+//
+//        gm.removeEdge("a","e");
+//
+//        gm.toString();
+//
+//        gm.outputDOTGraph("outputDOTGraphTest");
+//        gm.outputDOTGraph("checkDOTgraph");
+//
+//        gm.outputGraphics("opGraphicsTest","png");
+//        gm.outputGraphics("opGraphicsTest1a","SVG");
+//
+//        gm.outputGraph("outputTestGraph");
 
     }
 
--- src/main/java/aramak21/GraphManager.java
+++ src/main/java/aramak21/GraphManager.java
@@ -1,11 +1,13 @@
 package aramak21;
 
+import guru.nidi.graphviz.engine.Format;
+import guru.nidi.graphviz.engine.Graphviz;
 import guru.nidi.graphviz.model.MutableGraph;
 import guru.nidi.graphviz.parse.Parser;
 
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
+import java.util.Objects;
 
 import static guru.nidi.graphviz.model.Factory.mutGraph;
 import static guru.nidi.graphviz.model.Factory.mutNode;
@@ -31,15 +33,16 @@ public class GraphManager {
      System.out.println("The number of nodes in the graphs is " + graph.nodes.size());
      System.out.println("The number of edges in the graph is " + graph.edges.size());
      System.out.println("The label is " + graph.label);
-
+     System.out.println("The nodes are ");
      for(Node node : graph.nodes)
-         System.out.println("The node is " + node.getName());
-
+         System.out.print(" " + node.getName() + " ");
+     System.out.println("\n");
+     System.out.println("The edges are from : ");
      for(Edge edge : graph.edges)
-         System.out.println("The edge is from " + edge.getSource().getName() + " -> " + edge.getTarget().getName());
+         System.out.println( edge.getSource().getName() + " -> " + edge.getTarget().getName());
 
      output = graphToMutableGraph().toString();
-    System.out.println("op file is " + output);
+    System.out.println(" \n op file is " + output);
      return output;
     }
 
@@ -51,11 +54,15 @@ public class GraphManager {
         {
             if(n.getName().equals(node_name))
             {
-                System.out.println("the node " + node_name + " already exists"); return;
+                System.out.println("\n the node " + node_name + " already exists"); return;
             }
         }
         graph.nodes.add(new Node(node_name));
-        System.out.println("The number of nodes is " + graph.nodes.size());
+        System.out.println("\nThe number of nodes after adding " + node_name+ " is " + graph.nodes.size());
+        System.out.println("\nthe nodes after add operation ");
+        for(Node node : graph.nodes)
+            System.out.print(" " + node.getName() + " ");
+
     }
     public void addNodes(String[] node) {
         for (String n:node) {
@@ -97,12 +104,15 @@ public class GraphManager {
     {
         if(e.getSource().getName().equals(source) && e.getTarget().getName().equals(target))
         {
-            System.out.println("the edge with source "+ source + "and target " + target + " already exists");
+            System.out.println("\n the edge with source "+ source + "and target " + target + " already exists");
             return;
         }
     }
     graph.edges.add(new Edge(s,t));
 
+        System.out.println("\nThe edges are from : ");
+        for(Edge edge : graph.edges)
+            System.out.println( edge.getSource().getName() + " -> " + edge.getTarget().getName());
 
     }
     public void removeNode(String node_name) {
@@ -117,8 +127,11 @@ public class GraphManager {
             nodes1.add(n);
 
         }
-        System.out.println(" the size of list after removing the node is " + nodes1.size());
+        System.out.println(" \nthe size of list after removing the node is " + nodes1.size());
         graph.nodes = nodes1;
+        System.out.println("\nthe nodes after the remove operation ");
+        for(Node node : graph.nodes)
+            System.out.print(" " + node.getName() + " ");
 
         for(Edge e : graph.edges)
         {
@@ -147,6 +160,11 @@ public class GraphManager {
             edges1.add( new Edge(e.getSource(),e.getTarget()));
         }
         graph.edges= edges1 ;
+
+        System.out.println("\nthe number of edges after the delete operation is "+ graph.edges.size());
+        System.out.println("The edges are from : ");
+        for(Edge edge : graph.edges)
+            System.out.println( edge.getSource().getName() + " -> " + edge.getTarget().getName());
     }
 
 
@@ -167,4 +185,36 @@ public class GraphManager {
 
         return g;
     }
+
+    public void outputDOTGraph(String path) throws IOException
+    {
+        MutableGraph mutableGraph = graphToMutableGraph();
+        Graphviz.fromGraph(mutableGraph).width(800).render(Format.DOT).toFile(new File(path));
+    }
+
+    public void outputGraphics(String path, String format) throws IOException
+    {
+        MutableGraph mutableGraph = graphToMutableGraph();
+
+        if(Objects.equals(format,"png")||Objects.equals(format,"PNG"))
+            Graphviz.fromGraph(mutableGraph).width(900).render(Format.PNG).toFile(new File(path));
+
+        if(Objects.equals(format,"svg")||Objects.equals(format,"SVG"))
+            Graphviz.fromGraph(mutableGraph).width(900).render(Format.SVG).toFile(new File(path));
+    }
+
+    public void outputGraph(String filepath) throws IOException
+    {
+
+        BufferedWriter bw = new BufferedWriter(new FileWriter(filepath));
+        bw.write("\nThe number of Nodes is " + graph.nodes.size());
+        bw.write("\nThe number of edges is " + graph.edges.size());
+        bw.write(" \n The nodes are as follows " );
+        for(Node n: graph.nodes)
+            bw.write(n.getName() + " " );
+        bw.write("\n the edges are as follows " + graphToMutableGraph());
+        bw.close();
+    }
+
+
 }
