7e47162786e706afc25232dbe91f4413a697b451
Abhishek-Ram25 <aramak21@asu.edu>
2023-04-18 22:16:13 -0700

    Rename all occurances of variable 'g' in GraphManagerTest to graphManager to improve readability

--- src/test/java/aramak21/GraphManagerTest.java
+++ src/test/java/aramak21/GraphManagerTest.java
@@ -9,113 +9,111 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 
-import static org.junit.Assert.*;
-
 public class GraphManagerTest {
-    GraphManager g;
+    GraphManager graphManager;
 
     @Before
     public void setup() throws Exception {
-        g = new GraphManager();
-        g.parseGraph("testInputGraph.dot");
+        graphManager = new GraphManager();
+        graphManager.parseGraph("testInputGraph.dot");
     }
 
     @Test
     public void testParseGraph() {
-        Assert.assertEquals(4, g.nodeSize());
-        Assert.assertEquals(4, g.edgeSize());
-        Assert.assertTrue(g.containsEdge("a", "b"));
-        Assert.assertTrue(g.containsEdge("b", "c"));
-        Assert.assertTrue(g.containsEdge("c", "d"));
-        Assert.assertTrue(g.containsEdge("d", "a"));
-        System.out.println(g.toString());
+        Assert.assertEquals(4, graphManager.nodeSize());
+        Assert.assertEquals(4, graphManager.edgeSize());
+        Assert.assertTrue(graphManager.containsEdge("a", "b"));
+        Assert.assertTrue(graphManager.containsEdge("b", "c"));
+        Assert.assertTrue(graphManager.containsEdge("c", "d"));
+        Assert.assertTrue(graphManager.containsEdge("d", "a"));
+        System.out.println(graphManager.toString());
     }
 
     @Test
     public void testAddNode() throws Exception {
-        Assert.assertEquals(4, g.nodeSize());
-        g.addNode("e");
-        System.out.println(g.toString());
+        Assert.assertEquals(4, graphManager.nodeSize());
+        graphManager.addNode("e");
+        System.out.println(graphManager.toString());
 
-        Assert.assertEquals(5, g.nodeSize());
-        Assert.assertTrue(g.containsNode("e"));
+        Assert.assertEquals(5, graphManager.nodeSize());
+        Assert.assertTrue(graphManager.containsNode("e"));
 
-       g.addNode("a");
-        Assert.assertEquals(5, g.nodeSize());
-        Assert.assertTrue(g.containsNode("a"));
+       graphManager.addNode("a");
+        Assert.assertEquals(5, graphManager.nodeSize());
+        Assert.assertTrue(graphManager.containsNode("a"));
 
     }
 
     @Test
     public void testAddNodes() throws Exception{
-        Assert.assertEquals(4, g.nodeSize());
-        g.addNodes(new String[]{"e", "f", "g"});
-        Assert.assertEquals(7, g.nodeSize());
+        Assert.assertEquals(4, graphManager.nodeSize());
+        graphManager.addNodes(new String[]{"e", "f", "g"});
+        Assert.assertEquals(7, graphManager.nodeSize());
 
-        g.addNodes(new String[]{"a", "b", "c"});
-        Assert.assertEquals(7, g.nodeSize());
+        graphManager.addNodes(new String[]{"a", "b", "c"});
+        Assert.assertEquals(7, graphManager.nodeSize());
     }
 
     @Test
     public void testAddEdge() throws Exception {
-        Assert.assertEquals(4, g.edgeSize());
-        g.addEdge("e", "a");
+        Assert.assertEquals(4, graphManager.edgeSize());
+        graphManager.addEdge("e", "a");
        // System.out.println(g.toString());
-        Assert.assertEquals(5, g.edgeSize());
-        Assert.assertEquals(5, g.nodeSize());
-        Assert.assertTrue(g.containsNode("e"));
-        Assert.assertTrue(g.containsEdge("e", "a"));
+        Assert.assertEquals(5, graphManager.edgeSize());
+        Assert.assertEquals(5, graphManager.nodeSize());
+        Assert.assertTrue(graphManager.containsNode("e"));
+        Assert.assertTrue(graphManager.containsEdge("e", "a"));
 
-        g.addEdge("a","b");
-        Assert.assertEquals(5, g.edgeSize());
-        Assert.assertEquals(5, g.nodeSize());
-        Assert.assertTrue(g.containsEdge("a", "b"));
+        graphManager.addEdge("a","b");
+        Assert.assertEquals(5, graphManager.edgeSize());
+        Assert.assertEquals(5, graphManager.nodeSize());
+        Assert.assertTrue(graphManager.containsEdge("a", "b"));
     }
 
     @Test
     public void testRemoveNode() throws Exception {
-        Assert.assertEquals(4, g.nodeSize());
-        g.removeNode("a");
+        Assert.assertEquals(4, graphManager.nodeSize());
+        graphManager.removeNode("a");
 
-        Assert.assertEquals(3, g.nodeSize());
-        Assert.assertFalse(g.containsNode("a"));
+        Assert.assertEquals(3, graphManager.nodeSize());
+        Assert.assertFalse(graphManager.containsNode("a"));
 
-        g.removeNode("f");
-        Assert.assertEquals(3,g.nodeSize());
+        graphManager.removeNode("f");
+        Assert.assertEquals(3, graphManager.nodeSize());
     }
 
 
     @Test
     public void testRemoveNodes() throws Exception{
-        Assert.assertEquals(4, g.nodeSize());
-        g.removeNodes(new String[]{"e", "f", "g"});
-        Assert.assertEquals(4, g.nodeSize());
+        Assert.assertEquals(4, graphManager.nodeSize());
+        graphManager.removeNodes(new String[]{"e", "f", "g"});
+        Assert.assertEquals(4, graphManager.nodeSize());
 
-        g.removeNodes(new String[]{"a", "b", "c"});
-        Assert.assertEquals(1, g.nodeSize());
+        graphManager.removeNodes(new String[]{"a", "b", "c"});
+        Assert.assertEquals(1, graphManager.nodeSize());
 
     }
 
     @Test
     public void testRemoveEdge() {
-        Assert.assertEquals(4, g.edgeSize());
-        g.removeEdge("b", "c");
+        Assert.assertEquals(4, graphManager.edgeSize());
+        graphManager.removeEdge("b", "c");
        // System.out.println(g.toString());
 
-        Assert.assertEquals(4, g.nodeSize());
-        Assert.assertEquals(3, g.edgeSize());
+        Assert.assertEquals(4, graphManager.nodeSize());
+        Assert.assertEquals(3, graphManager.edgeSize());
 
-        Assert.assertEquals(3, g.edgeSize());
-        g.removeEdge("a","c");
-        Assert.assertEquals(4, g.nodeSize());
-        Assert.assertEquals(3, g.edgeSize());
+        Assert.assertEquals(3, graphManager.edgeSize());
+        graphManager.removeEdge("a","c");
+        Assert.assertEquals(4, graphManager.nodeSize());
+        Assert.assertEquals(3, graphManager.edgeSize());
     }
 
     @Test
     public void testOutputDOTGraph() throws IOException {
-        g.addEdge("e", "f");
+        graphManager.addEdge("e", "f");
         String outputfile = "output2.dot";
-        g.outputDOTGraph(outputfile);
+        graphManager.outputDOTGraph(outputfile);
 
         List<String> output = Files.readAllLines(Paths.get(outputfile));
         System.out.println("output: " + output);
@@ -127,7 +125,7 @@ public class GraphManagerTest {
     public void testOutputGraph() throws IOException
     {
         String output_graph_name = "testOPGraph.txt";
-        g.outputGraph(output_graph_name);
+        graphManager.outputGraph(output_graph_name);
         List<String> output = Files.readAllLines(Paths.get(output_graph_name));
         System.out.println("output: " + output_graph_name);
         List<String> expected = Files.readAllLines(Paths.get("expectedOutputGraph.txt"));
@@ -137,48 +135,48 @@ public class GraphManagerTest {
 
     @Test
     public void testDFSGraphSearch() {
-        Path path = g.GraphSearch(new Node("a"), new Node("d"), Algorithm.dfs );
+        Path path = graphManager.GraphSearch(new Node("a"), new Node("d"), Algorithm.dfs );
         Assert.assertEquals("a -> b -> c -> d", path.toString());
 
-        path = g.GraphSearch(new Node("a"), new Node("a") , Algorithm.dfs);
+        path = graphManager.GraphSearch(new Node("a"), new Node("a") , Algorithm.dfs);
         Assert.assertEquals("a", path.toString());
 
 
-        path = g.GraphSearch(new Node("b"), new Node("a") , Algorithm.dfs);
+        path = graphManager.GraphSearch(new Node("b"), new Node("a") , Algorithm.dfs);
         Assert.assertEquals("b -> c -> d -> a", path.toString());
 
-        g.addNodes(new String[]{"e","f","g", "h", "i"});
-        g.addEdge("d","e");
-        g.addEdge("d","f");
-        g.addEdge("d","g");
-        g.addEdge("d","h");
-        g.addEdge("g","h");
-        g.addEdge("h","i");
+        graphManager.addNodes(new String[]{"e","f","g", "h", "i"});
+        graphManager.addEdge("d","e");
+        graphManager.addEdge("d","f");
+        graphManager.addEdge("d","g");
+        graphManager.addEdge("d","h");
+        graphManager.addEdge("g","h");
+        graphManager.addEdge("h","i");
 
-        path = g.GraphSearch(new Node("a"), new Node("i") , Algorithm.dfs);
+        path = graphManager.GraphSearch(new Node("a"), new Node("i") , Algorithm.dfs);
         Assert.assertEquals("a -> b -> c -> d -> g -> h -> i", path.toString());
     }
 
     @Test
     public void testBFS(){
         {
-            Path path = g.GraphSearch(new Node("a"), new Node("d"), Algorithm.bfs);
+            Path path = graphManager.GraphSearch(new Node("a"), new Node("d"), Algorithm.bfs);
             Assert.assertEquals("a -> b -> c -> d", path.toString());
 
-            path = g.GraphSearch(new Node("a"), new Node("a"), Algorithm.bfs);
+            path = graphManager.GraphSearch(new Node("a"), new Node("a"), Algorithm.bfs);
             Assert.assertEquals("a", path.toString());
 
 
-            path = g.GraphSearch(new Node("b"), new Node("a"), Algorithm.bfs);
+            path = graphManager.GraphSearch(new Node("b"), new Node("a"), Algorithm.bfs);
             Assert.assertEquals("b -> c -> d -> a", path.toString());
 
-            g.addNodes(new String[]{"e", "f", "g"});
-            g.addEdge("d","e");
-            g.addEdge("e","f");
-            g.addEdge("d","f");
-            g.addEdge("f","g");
+            graphManager.addNodes(new String[]{"e", "f", "g"});
+            graphManager.addEdge("d","e");
+            graphManager.addEdge("e","f");
+            graphManager.addEdge("d","f");
+            graphManager.addEdge("f","g");
 
-            path = g.GraphSearch(new Node("a"), new Node("g"), Algorithm.bfs);
+            path = graphManager.GraphSearch(new Node("a"), new Node("g"), Algorithm.bfs);
             Assert.assertEquals("a -> b -> c -> d -> " +
                     "f -> g", path.toString());
         }
